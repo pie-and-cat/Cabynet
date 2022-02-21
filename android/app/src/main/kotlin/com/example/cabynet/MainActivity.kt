5dc37e4d5e85acae8b8c20d7a9aa21ebb621ac2d
kasenra <54212521+kasenra@users.noreply.github.com>
2022-02-21 16:38:47 -0600

    Basic Skeleton Code
    Minor bit of code missing to be added in second commit

---
+++ android/app/src/main/kotlin/com/example/cabynet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cabynet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\latin\\Desktop\\Code\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\latin\\Desktop\\Code\\Flutter"
+  "PROJECT_DIR=C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet"
+  "FLUTTER_ROOT=C:\\Users\\latin\\Desktop\\Code\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet"
+  "FLUTTER_TARGET=C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\latin\\Desktop\\Code\\Spring 2022\\3380\\Project\\cabynet\\.dart_tool\\package_config.json"
+)
